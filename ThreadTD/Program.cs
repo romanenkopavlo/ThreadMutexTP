73bd6b5576c7e30a8a07fc4361afcf90b9e7f3a2
promanenko <paihitetty444@gmail.com>
2024-11-27 16:38:48 +0100

    Add project files.

---
+++ ThreadTD/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO.IsolatedStorage;
+
+namespace ThreadTD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Mutex m = new Mutex();
+            Random random = new Random();
+            Work work;
+
+            StreamWriter sw = new StreamWriter("thread.txt");
+            sw.WriteLine("Hello world");
+           
+            for (int i = 0; i < 10; i++)
+            {
+                work = new Work();
+                work.sw = sw;
+                work.mut = m;
+                work.random = random;
+                Thread thread = new Thread(work.Run);
+                thread.Start();
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ ThreadTD/Work.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadTD
+{
+    public class Work
+    {
+        public Mutex mut;
+        public StreamWriter sw;
+        public Random random;
+
+        public void Run() {
+            for (int i = 0; i < 10; i++)
+            {
+                mut.WaitOne();
+                Thread.Sleep(random.Next(0, 10));
+                sw.WriteLine("Ligne " + (i + 1));
+                mut.ReleaseMutex();
+            }
+        }
+    }
+}
